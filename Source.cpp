e1de1187064262376d1cac187117735fed4f12ae
DanilSupreme <59294947+CeRBeR-DeV@users.noreply.github.com>
2020-05-25 13:05:27 +0300

    commit

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-#include <conio.h>
 #include <ctime>
+#include <conio.h>
 
 void Start(char GeneratedArray[], char UserArray[], int n);
 void Fill_Rand(char arr[], int n);
 void Print(char arr[], int n);
-void Enter(char user[], char compuer[], int n);
+void Enter(char user[], char rand[], int n);
 void score(char user[], char compuer[], int n);
 unsigned int Time = 0;
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	std::cout << "Клавиатурный тренажер\nУбедитесь что включен Caps Lock и английская раскладка клавиатуры\nУдачной тренировки :)\n";
+	std::cout << "Клавиатурный тренажер\nBackspace не работает\nУбедитесь что включен Caps Lock и английская раскладка клавиатуры\nУдачной тренировки :) :(\n\n";
 	srand(time(NULL));
-	const int n = 49;
+	const int n = 40;
 	char GeneratedArray[n];
 	char UserArray[n];
 	Start(GeneratedArray, UserArray, n);
@@ -34,7 +34,7 @@ void Print(char arr[], int n)
 {
 	for (int i = 0; i < n; i++)std::cout << arr[i];
 }
-void Enter(char user[], char compuer[], int n)
+void Enter(char user[], char rand[], int n)
 {
 	unsigned int start_time = clock();
 	std::cout << std::endl << std::endl;
@@ -42,7 +42,7 @@ void Enter(char user[], char compuer[], int n)
 	for (int i = 0; i < n; i++)
 	{
 		user[i] = _getch();
-	    if (user[i] == compuer[i])std::cout << "\033[32m\033[1m" << user[i] << "\033[0m";
+	    if (user[i] == rand[i])std::cout << "\033[32m\033[1m" << user[i] << "\033[0m";
 		else std::cout << "\033[31m\033[1m" << user[i] << "\033[0m";
 	}
 	//
@@ -50,12 +50,12 @@ void Enter(char user[], char compuer[], int n)
 	unsigned int search_time = end_time - start_time;
 	Time = search_time;
 }
-void score(char user[], char compuer[], int n)
+void score(char user[], char rand[], int n)
 {
-	int incorrect = 0, correct = 0;
-	for (int i = 0; i < n; i++)user[i] != compuer[i] ? incorrect++ : correct++;
-	std::cout << "\n\n\nПравильно: " << correct;
-	std::cout << "\n\nОшибок: " << incorrect;
+	int neverno = 0, verno = 0;
+	for (int i = 0; i < n; i++)user[i] != rand[i] ? neverno++ : verno++;
+	std::cout << "\n\n\nПравильно: " << verno;
+	std::cout << "\n\nОшибок: " << neverno;
 	int Sec = Time / 1000;
 	int MSec = Time - (Sec * 1000);
 	std::cout << "\n\nВремя: " << Sec << "," << MSec << " секунд";
